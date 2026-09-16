7c1506155e0081aab8185718b66b4ee88a205cf9
shubhamraj82 <raj.shubh8216@gmail.com>
2026-09-16 21:31:08 +0530

    feat: integrate Sentry for error tracking and performance monitoring
    - Added Sentry for Next.js to package.json dependencies.
    - Configured Sentry for edge and server environments with respective initialization files.
    - Implemented Sentry middleware in TRPC procedures for error capturing.
    - Enhanced generation router to log events and errors to Sentry.
    - Created example API route and page to demonstrate Sentry's error monitoring capabilities.
    - Added global error handling component to capture exceptions and report to Sentry.
    - Configured client-side instrumentation for enhanced error tracking and performance monitoring.

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e9ff1ef250965e0e370bd74fbb2b4633@o4511700423868416.ingest.us.sentry.io/4512096759906304",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,3 +1,4 @@
+import *as Sentry from "@sentry/node";
 import { auth } from '@clerk/nextjs/server';
 import { initTRPC, TRPCError } from '@trpc/server';
 import { cache } from 'react';
@@ -21,13 +22,19 @@ const t = initTRPC.create({
   transformer: superjson,
 });
  
+
+const sentryMiddleware = t.middleware(
+  Sentry.trpcMiddleware({
+    attachRpcInput: true,
+  }),
+);
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
-export const baseProcedure = t.procedure;
+export const baseProcedure = t.procedure.use(sentryMiddleware);
 
 // Authenticated procedure - calls auth() only when needed
-export const authProcedure = t.procedure.use(async ({ next }) => {
+export const authProcedure = baseProcedure.use(async ({ next }) => {
   const {userId} = await auth();
   if(!userId){
     throw new TRPCError({
@@ -41,7 +48,7 @@ export const authProcedure = t.procedure.use(async ({ next }) => {
 })
 
 // organization procedure - requires userId and orgId in context
-export const orgProcedure = t.procedure.use(async ({ next }) => {
+export const orgProcedure = baseProcedure.use(async ({ next }) => {
   const {userId, orgId} = await auth();
 
   if(!userId){
