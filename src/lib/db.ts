1c32cc6d3833db76a47716aa886ea3c8d8be2c46
shubhamraj82 <raj.shubh8216@gmail.com>
2026-09-25 22:06:04 +0530

    fix: correct spacing in connectionString assignment in db.ts for consistency

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import {PrismaPg} from "@prisma/adapter-pg"
 import { env } from "./env";
 
 const adapter = new PrismaPg ({
-    connectionString: env.DATABASE_URL,
+    connectionString: env.DATABASE_URL ,
 });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
